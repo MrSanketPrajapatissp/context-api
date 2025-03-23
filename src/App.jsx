22944b624b38ddac1ca794d7c688e2ee0c6d3160
MrSanketPrajapatissp <125129649+mrsanketprajapati@users.noreply.github.com>
2025-03-23 16:29:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React, { createContext, useContext, useState } from "react";
+
+const AppContext = createContext(null);
+
+export const Parent = () => {
+  const [UserName, setUserName] = useState("Sanekt131");
+
+  return (
+    <div>
+      <div> Username: {UserName}</div>
+      <AppContext.Provider value={{ UserName, setUserName }}>
+        <Child />
+      </AppContext.Provider>
+    </div>
+  );
+};
+
+export const Child = () => {
+  return (
+    <div>
+      <GrandChild />
+    </div>
+  );
+};
+
+export const GrandChild = () => {
+  const { setUserName } = useContext(AppContext);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setUserName("Harry$324");
+        }}
+      >
+        Change userName
+      </button>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div>
+      <Parent />
+    </div>
+  );
+}
+
+export default App;
